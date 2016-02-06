5d5fc234e9b1e38e9d33ffb7439a5893658e4105
Soumendra kumar sahoo <soumendra.s@outlook.com>
2016-02-06 23:39:31 +0530

    Code updated

--- SOS.py
+++ SOS.py
@@ -1,6 +1,23 @@
-#Sending data:     'SOSSOSSOSSOS'
-#Receiving data:   'SOSTOSSOSQOS'
-#Find the number of letters mismatched
-T = 2
-for _ in range(T):
-    
+# count = 0
+# S = raw_input()
+# for i in range(2,len(S),3):
+# 	if S[i-2:i] == 'SOS':
+# 		pass
+# 	else:
+# 		if (S[i] != 'S'): 
+# 			count += 1
+# 		if (S[i-1] != 'O'):  
+# 			count += 1
+# 		if (S[i-2] != 'S'):
+# 			count += 1
+# print count
+
+S=raw_input()
+assert len(S)%3==0 and len(S)<=99
+n=len(S)/3
+exp="SOS"*n #Expected string
+ans=0
+for i in range(len(S)):
+    if exp[i]!=S[i]:
+        ans=ans+1
+print ans
